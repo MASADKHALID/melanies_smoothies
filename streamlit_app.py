bce86868f4e72d9ea1c707cd54f31e9fdbf28f93
Muhammad Asad Khalid <104402634+MASADKHALID@users.noreply.github.com>
2024-09-03 07:19:09 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,22 +2,32 @@
 import streamlit as st
 import requests
 from snowflake.snowpark.functions import col
+
 # Write directly to the app
-st.title(":cup_with_straw:Customize Your Smoothie!:cup_with_straw")
+st.title("Example Streamlit App :balloon:")
 st.write(
-    """choose the fruit you want in your custome Smoothie"""
+    """Replace this example with your own code!
+    **And if you're new to Streamlit,** check
+    out our easy-to-follow guides at
+    [docs.streamlit.io](https://docs.streamlit.io).
+    """
 )
+
+
+name_an_order = st.text_input('Name on Smoothie:')
+st.write("The name on your smoothie will be:", name_an_order)
+
+
 
 cnx =st.connection("snowflake")
 session=cnx.session()
-
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list=st.multiselect(
         'choose up to 5 ingredients:',
-        my_dataframe
-        ,max_selection=5
+        my_dataframe,
+        max_selections=5
 )
 if ingredients_list:
     ingredients_string=''
@@ -29,9 +39,10 @@ if ingredients_list:
         fv_df=st.dataframe(data=ruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
 
+
  
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-    values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+    values ('""" + ingredients_string + """','"""+name_an_order+"""')"""
     
     #st.write(my_insert_stmt)
     time_to_insert=st.button('submit order')
@@ -39,6 +50,7 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
 
 
 
